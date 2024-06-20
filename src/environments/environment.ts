7b40865b29ce7d14487ab0bc0d1ecbea8dab1ae4
Adriano da Silva Santos <adriano.b.santos@capgemini.com>
2024-06-20 11:20:04 -0300

    Stop tracking folder env

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-// environment.ts
-export const environment = {
-  production: false,
-  omdbApiKey: 'e24e8a5c',
-  apiUrlJavaSpring: 'http://localhost:8080',
-  apiUrlNodeJsExpress: 'http://localhost:3000'
-
-};
